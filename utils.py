0d1ab36b8e0595a175ae127128e5da5f3715b63a
Vinícius Almeida <almeidabarrosdf@gmail.com>
2023-11-17 07:57:50 -0300

    testing neurokit

--- utils.py
+++ utils.py
@@ -1,9 +1,5 @@
 import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
 import wfdb
-from glob import glob
-from os.path import join
 
 pd.set_option('display.max_columns', None)
 mb_artm_directory = './databases/mit-bih-arrhythmia-database-1.0.0/'
@@ -12,8 +8,8 @@ dataframes_directory = './Results/dataframes/'
 figures_directory = './Results/figures/'
 
 
-def load_record(record:int, files_directory = mb_artm_directory,**kwargs):
+def load_record(record: int, files_directory=mb_artm_directory, **kwargs):
     record_path = f'{files_directory}{record}'
     record = wfdb.rdrecord(record_path, **kwargs)
-    ann = wfdb.rdann(record_path,'atr', **kwargs)
+    ann = wfdb.rdann(record_path, 'atr', **kwargs)
     return record, ann
