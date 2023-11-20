e6e28f1a463b079983165834b92e51c05c3a67cd
Vinícius Almeida <almeidabarrosdf@gmail.com>
2023-11-20 11:24:22 -0300

    add globals

---
+++ globals.py
@@ -0,0 +1,3 @@
+INDEX_TYPE = "uint32[pyarrow]"
+ECG_TYPE = "float32[pyarrow]"
+ANN_TYPE = "string[pyarrow]"
--- utils.py
+++ utils.py
@@ -1,9 +1,7 @@
 import pandas as pd
 import wfdb
 import neurokit2 as nk
-
-import numpy as np
-import pyarrow as pa
+from globals import INDEX_TYPE
 
 
 pd.set_option('display.max_columns', None)
@@ -21,5 +19,32 @@ def load_record(record: int, files_directory=mb_artm_directory, **kwargs):
 
 
 def find_peaks(ecg, sampling_rate, method):
-    signal, info = nk.ecg_peaks(ecg, sampling_rate=sampling_rate, method=method)
+    signal, info = nk.ecg_peaks(ecg, sampling_rate=sampling_rate, method= method)
     return info["ECG_R_Peaks"]
+
+
+def rolling_idxmax(series : pd.Series, window_size : int, closed : str = 'both', center : bool = True, **kwargs)->pd.Series:
+    """
+    Calculate the index of the maximum value in a rolling window.
+    """
+    # TODO: implement it using vectorized operations
+    return series.rolling(window_size, closed = closed, center = center, **kwargs).apply(lambda x: x.idxmax()).dropna(ignore_index = False).astype(INDEX_TYPE).reset_index().rename(columns = {0: 'local_max'})
+
+
+def correct_peaks(ecg : pd.Series, ann_beat_indexes, fs, window_size = 0.28 + 0.120, **kwargs):
+    """
+    Correct peaks by finding local maxima around the annotated peaks.
+    Some methods may fail to detect the correct peaks in the begining of the signal. 
+    """
+
+    # TODO: make it more efficient by finding maxima only around ann_beat_indexes
+    ecg = pd.Series(ecg)
+
+    df_beats = pd.DataFrame({'index' : ann_beat_indexes})
+
+    # Find local maxima
+    rolling_idxmax_df = rolling_idxmax(ecg, int(fs * window_size), **kwargs)
+    
+    df_beats =  df_beats.merge(rolling_idxmax_df, how = 'left', on = 'index')
+
+    return df_beats
